420ef073d0a87d03800b80eddbb56dcef9dca92c
Aradhya Nepal <aradhya.1221@gmail.com>
2023-07-20 10:01:36 +0545

    Dio base repository under way

--- packages/dio_base_by_a2/lib/src/base_repository.dart
+++ packages/dio_base_by_a2/lib/src/base_repository.dart
@@ -1,29 +1,56 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
+typedef ResponseManipulator = String Function(Map);
 
 class BaseRepository {
-  ///Defaults to true,
-  ///If this is set true, in all request, token is passed in all of the request
-  //Todo: Make it private
-  final bool repoNeedToken;
+  final bool _isTokenBasedRepo;
 
+  ///Defaults to true,
+  ///If this is set true, token is passed in all of the request
+  bool get isTokenBasedRepo => _isTokenBasedRepo;
+
+  final int _repoTimeout;
+
+  ///On Dio request, its the duration after which the repo hits the timeout
+  ///which is measured in seconds
+  ///
   ///If nothing is set, then its 60 seconds
-  //Todo: Make it private
-  final int repoTimeout;
+  int get repoTimeout => _repoTimeout;
   final Dio _client;
 
+  final ResponseManipulator _responseManipulate;
+  ///In most case backend return a generic response,
+  ///like:
+  ///
+  /// {
+  ///
+  ///   ...
+  ///
+  ///   "data":{
+  ///       ... Actual Result that we need
+  ///   }
+  ///
+  /// }
+  ///
+  /// This method is used to manipulate those response on every return so that
+  /// for every method below, we will only get the value inside the data.
+  ResponseManipulator get responseManipulate=>_responseManipulate;
 
   ///In order to do the testing, you can pass Mocked Dio on [dio].
   ///
   /// For Security purpose, the logs of the network dio request will only will
   /// displayed on debug and profile mode, those logs will on work on release.
   BaseRepository({
-    this.repoNeedToken = true,
-    this.repoTimeout = 60,
+    bool isTokenBasedRepo = true,
+    int repoTimeout = 60,
+    ResponseManipulator responseManipulate = _defaultResponseManipulator,
     Dio? dio,
-  }):_client=dio??Dio(){
-    if(!kReleaseMode){
+  })  : _client = dio ?? Dio(),
+        _isTokenBasedRepo = isTokenBasedRepo,
+        _repoTimeout = repoTimeout,
+        _responseManipulate = responseManipulate {
+    if (!kReleaseMode) {
       _client.interceptors.add(
         LogInterceptor(
           responseBody: true,
@@ -35,8 +62,11 @@ class BaseRepository {
     }
   }
 
-  ///On not passing [haveToken], [repoNeedToken]  value will be used
-  ///On not passing [timeoutSeconds], [repoTimeout]  value will be used
+  static String _defaultResponseManipulator(Map map) {
+    return map["data"];
+  }
+
+  ///On not passing [haveToken], [isTokenBasedRepo]  value will be used
   Future<T> get<T>(
     String url, {
     Map? header,
@@ -44,7 +74,8 @@ class BaseRepository {
     bool? haveToken,
     int? timeoutSeconds,
   }) async {
-    haveToken ??= repoNeedToken;
+    haveToken ??= isTokenBasedRepo;
+    timeoutSeconds ??= repoTimeout;
     return throw UnimplementedError();
   }
 
