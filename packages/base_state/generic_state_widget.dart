e3028c691ec26376290809b02c1e3b9e26485260
Aradhya Nepal <aradhya.1221@gmail.com>
2023-10-08 16:52:25 +0545

    Working on pagination and isEmpty

--- packages/base_state/generic_state_widget.dart
+++ packages/base_state/generic_state_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import 'generic_state.dart';
@@ -10,6 +9,10 @@ class GenericStateWidget<T> extends StatelessWidget {
   final Future<void> Function() onErrorReload;
   final Future<void> Function()? onRefresh;
   final GenericState<T> state;
+  final bool Function(SuccessState<T>)? isEmptyCheck;
+  ///[isEmptyCheck] value must be true to display this widget.
+  ///If widget not passed "No result" text will be shown.
+  final Widget Function(SuccessState<T>)? onEmpty;
 
   const GenericStateWidget({
     super.key,
@@ -19,6 +22,8 @@ class GenericStateWidget<T> extends StatelessWidget {
     this.loadingShimmer,
     this.onRefresh,
     this.onError,
+    this.isEmptyCheck,
+    this.onEmpty,
   });
 
   @override
@@ -28,7 +33,7 @@ class GenericStateWidget<T> extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     final state=this.state;
     Widget outputChild = switch (state) {
-      SuccessState<T>() => onSuccess(state),
+      SuccessState<T>()=>_onSuccessWidget(state,textTheme),
       ErrorState<T>() => onError?.call(state) ??
           Column(
             mainAxisSize: MainAxisSize.min,
@@ -41,7 +46,7 @@ class GenericStateWidget<T> extends StatelessWidget {
                 child: TextButton(
                   onPressed: onErrorReload,
                   child: Text(
-                    "Reload",
+                    "Reload",//Todo: Add Locale
                     style: textTheme.displaySmall?.copyWith(
                       fontWeight: FontWeight.bold,
                     ),
@@ -65,5 +70,70 @@ class GenericStateWidget<T> extends StatelessWidget {
     } else {
       return outputChild;
     }
+  }
+
+  Widget _onSuccessWidget(SuccessState<T> state,TextTheme textTheme,){
+    if(isEmptyCheck?.call(state)==true){
+      return onEmpty?.call(state)??Text(
+        "No Result",//Todo: Add locale
+        style: textTheme.displaySmall?.copyWith(
+          fontWeight: FontWeight.bold,
+        ),
+      );
+    }else{
+      return onSuccess(state);
+    }
+  }
+}
+
+
+class GenericStatePaginationWidget<T> extends StatefulWidget {
+  final Widget Function(SuccessState<T>) onSuccess;
+  final Widget Function(ErrorState<T>)? onError;
+  final VoidCallback fetchNextPage;
+  final ScrollController scrollController;
+  final Widget Function()? loadingShimmer;
+  final Future<void> Function() onErrorReload;
+  final Future<void> Function()? onRefresh;
+  final GenericState<T> state;
+  const GenericStatePaginationWidget({
+    super.key,
+    required this.state,
+    required this.onSuccess,
+    required this.onErrorReload,
+    required this.scrollController,
+    required this.fetchNextPage,
+    this.loadingShimmer,
+    this.onRefresh,
+    this.onError,
+  });
+
+  @override
+  State<GenericStatePaginationWidget<T>> createState() => _GenericStatePaginationWidgetState<T>();
+}
+
+class _GenericStatePaginationWidgetState<T> extends State<GenericStatePaginationWidget<T>> {
+
+  @override
+  void initState() {
+    super.initState();
+    widget.scrollController.addListener(scrollListener);
+  }
+
+  void scrollListener(){
+    if(!context.mounted)return;
+    if(widget.state.canDoPagination(widget.scrollController)){
+      widget.fetchNextPage();
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  @override
+  void dispose() {
+    widget.scrollController.removeListener(scrollListener);
+    super.dispose();
   }
 }
