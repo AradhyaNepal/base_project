a8a163c282177923bc3a2628221a242b4f752ce9
Aradhya Nepal <aradhya.1221@gmail.com>
2023-10-08 11:11:48 +0545

    Adding generic states

--- packages/dio_base_by_a2/example/dio_base_by_a2_example.dart
+++ packages/dio_base_by_a2/example/dio_base_by_a2_example.dart
@@ -1,16 +1,39 @@
+import 'dart:developer';
+
 import 'package:dio_base_by_a2/dio_base_by_a2.dart';
 
 void main() {}
 
 class PropertyRepository extends BaseRepository {
-  void getProperty() async {
-     await get(
-      RequestInput("google.com",
-          data: "",
-          params: {"avs": "fasdf"},
+  PropertyRepository()
+      : super(
           repositoryDetails: RepositoryDetails(
-            tokenNeeded: false,
-          )),
+              tokenNeeded: false,
+              requestTimeout: 10,
+              responseManipulate: (value) {
+                return value["bello"];
+              }),
+        );
+
+  void getProperty() async {
+    final value = await get<int>(
+      RequestInput(
+        "google.com",
+        data: "",
+        params: {"avs": "fasdf"},
+        repositoryDetails: RepositoryDetails(
+          tokenNeeded: true,
+          requestTimeout: 20,
+          responseManipulate: (value) {
+            return value["hello"];
+          },
+        ),
+      ),
     );
+    log(value.toString());
+  }
+
+  void deleteProperty() async {
+    await delete(RequestInput("abcd.google"));
   }
 }
